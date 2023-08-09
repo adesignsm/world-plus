795b2e77eb61087c11822d8f6743539db1ab0c8a
Akash Mulye <akashmulye97@gmail.com>
2023-08-09 12:39:46 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./root.css";
+
+import Background from "./Components/Background";
+
+const App = () => {
+    return (
+        <>
+            <Background />
+        </>
+    )
+}
+
+export default App;
---
+++ src/Components/Background/index.jsx
@@ -0,0 +1,24 @@
+import "./index.css";
+
+const Background = () => {
+    return (
+        <>
+            <div id="blob-container">
+                <div className="blob-container">
+                    <div className="shape-blob one"></div>
+                    <div className="shape-blob two"></div>
+                    <div className="shape-blob three"></div>
+                    <div className="shape-blob four"></div>
+                    <div className="shape-blob five"></div>
+                    <div className="shape-blob six"></div>
+                    <div className="shape-blob seven"></div>
+                    <div className="shape-blob eight"></div>
+                    <div className="shape-blob nine"></div>
+                    <div className="shape-blob ten"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Background;
